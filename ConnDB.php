d94dd17c2678eb32bc388641f5725eb2d43008a1
Tafadzwa <muzerenganit@gmail.com>
2019-10-28 22:44:05 +0200

    connDB

---
+++ ConnDB.php
@@ -0,0 +1,22 @@
+<?php 
+    define('SERVERNAME', 'localhost');
+    define('USERNAME', 'username');
+    define('PASSWORD', 'password');
+    define('DBNAME', 'databasename');
+
+    function connDB()
+    {
+        $servername = SERVERNAME;
+        $nme = USERNAME;
+        $pass = PASSWORD;
+        $dbName = DBNAME;
+        //connect database
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbName", $nme, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOEception $e) {
+            echo "Connection Failed".$e->newMessage();
+        }
+    }
+?>
