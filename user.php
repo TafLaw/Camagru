f871f887f6208a2c52eedf4223e0acaf5271af51
Tafadzwa <muzerenganit@gmail.com>
2019-10-29 00:12:02 +0200

    user.php

--- user.php
+++ user.php
@@ -9,7 +9,7 @@
                 $stmt = $conn->prepare("SELECT id FROM profiles WHERE (name=:usernameEmail or email=:usernameEmail) AND password=:passwd");
                 //protect against sql injection (stored as type char / varchar)
                 $stmt->bindParam("usernameEmail", $usernameEmail,PDO::PARAM_STR);
-                $stmt->bindParam("hash_password", $hash_password,PDO::PARAM_STR);
+                $stmt->bindParam("passwd", passwd,PDO::PARAM_STR);
                 $stmt->execute();
                 //count rows in stmt
                 $rows = stmt->rowCount();
@@ -31,9 +31,60 @@
         public function register($username, $email, $password)
         {
             try {
+                $code = substr($password, 0, 8);//verification code
+                $dat = date("d F Y");
                 $conn = connDB();
-            } catch (\Throwable $th) {
-                //throw $th;
+                $stmt = $conn->prepare("SELECT id FROM profiles WHERE username=:username OR email=:email");
+                $stmt = bindParam("ss", $username, $email);
+                $stmt->execute();
+                $rows = $stmt->rowCount();
+                if ($rows < 1)
+                {
+                    $stmt = $conn->prepare("INSERT INTO `profiles` (`id`, `name`, `email`, `image`, `password`, `code`, `varified`, `date`) VALUES ('0', '$username', '$email', '', '$password', '$code', '0', '$dat');");
+                    $stmt = bindParam("sss", $username, $email, $password);
+                    $stmt->execute();
+
+                    //email and verification
+                    $id = $conn->lastInsertId();
+                    $message = "Your varification code is $code\n";
+                    $to = $email;
+                    $subject = "This is your verification code for Camagru\n";
+                    $from = 'muzerenganit@gmail.com';
+                    $body='Your verification Code is '.$code.' Please Click On This link <a href="http://localhost:8080/Camagru/verify.php?id='.$id.'&code='.$code.'">http://localhost:8080/Camagru/verify.php?id='.$uid.'&code='.$code.'</a> to activate your account.';
+                    /* $body = "<a href='google.com'>register</a>"; */
+                    $headers = "From:".$from."\r\n";
+                    $headers .= "MIME-Version: 1.0\r\n";
+                    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+                    if(mail($to,$subject,$body,$headers))
+                        include("thankYou.html");
+                    else
+                        echo '<h3 style="color:red;">failed to sent email</h3>';
+                    //conn to null
+                    $_SESSION['id'] = $id;
+                    return true;
+                }
+                else
+                {
+                    $conn = NULL;
+                    return false;
+                }
+            } catch (PDOException $e) {
+                echo "An error has occured".$e->getMesssage();
+            }
+        }
+
+        //user details
+        function details($id)
+        {
+            try {
+                $conn = connDB();
+                $stmt = $conn->prepare("SELECT name, email FROM profile WHERE id='$id'");
+                $stmt = bindParam('i', $id);
+                $stml->execute();
+                $data = $stml->fetch(PDO::FETCH_OBJ); //fetch user data
+                return $data;
+            } catch (PDOException $e) {
+                echo "An error has occured\n".$e->getMessage();
             }
         }
     }
