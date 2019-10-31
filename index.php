09b02e80b3e25a745c7050b95c62c05e19dbf31e
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-10-31 18:37:11 +0200

    index.php

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
     $dbName = DBNAME;
     include("connect/config.php");
 ?>
-<?php 
-    echo '<h1 style="color: skyblue">This is the home page\n</h1>';
-    echo '<button style="background-color: #7750bb; color:white; margin: 60px 0px 0px 45%;" onclick="window.location.href=\'authenticate/login_signup/signin_up.php\';">Back to Login</button></body>'
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Camagru</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <nav>
+            <div class="navBar">
+                <button style="background-color: #7750bb; color:white; margin: 60px 0px 0px 45%;" onclick="window.location.href='authenticate/login_signup/signin_up.php';">Back to Login</button></body>
+            </div>
+        </nav>
+    </body>
+</html>
