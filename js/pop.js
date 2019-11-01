58372f57222a7891fb45f6721d82809b62982e10
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-11-01 12:52:18 +0200

    editProfile.html

---
+++ js/pop.js
@@ -0,0 +1,38 @@
+function uploadBtn() 
+{
+    document.getElementById("uploadpop").classList.toggle("show");
+}
+function captureBtn() 
+{
+    document.getElementById("capturepop").classList.toggle("show");
+}
+
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.uploadbtn')) 
+    {
+        var pop = document.getElementsByClassName("uploadpop-cont");
+        var i;
+        for (i = 0; i < pop.length; i++)
+        {
+            var openPop = pop[i];
+            if (openPop.classList.contains('show'))
+            {
+                openPop.classList.remove('show');
+            }
+        }
+    }
+    else if (!event.target.matches('capturebtn')) 
+    {
+        var pop = document.getElementsByClassName("capturepop-cont");
+        var i;
+        for (i = 0; i < pop.length; i++)
+        {
+            var openPop = pop[i];
+            if (openPop.classList.contains('show'))
+            {
+                openPop.classList.remove('show');
+            }
+        }
+    }
+}
